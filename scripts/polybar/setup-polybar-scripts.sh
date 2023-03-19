c0443f1677257cef03d3b0fa684d8b3142d1c420
Liam Dyer <liamcdyer@gmail.com>
2023-03-19 15:25:14 -0400

    cleanup bspwm, remove unnecessary polybar

--- scripts/polybar/setup-polybar-scripts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd $(dirname "$0")
-chmod +x polybar-scripts/polybar-scripts/updates-pacman-aurhelper/updates-pacman-aurhelper.sh
-chmod +x polybar-scripts/polybar-scripts/openweathermap-simple/openweathermap-simple.sh
